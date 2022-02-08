6ae32afcd94942a07128290be789229a479b8675
Jennings Zhang <jenni_zh@protonmail.com>
2022-02-08 02:42:36 -0500

    Add exec to command

--- version_testing/test.sh
+++ version_testing/test.sh
@@ -8,5 +8,5 @@ fi
 python_version=$(docker inspect -f '{{ index .Config.Labels "org.chrisproject.python_version" }}' $1)
 >&2 printf "Running tests for version %s: " "$python_version"
 
-docker run --rm -t \
+exec docker run --rm -t \
   -v $(realpath ..)/chris_plugin:/usr/local/src/chris_plugin/chris_plugin $1
